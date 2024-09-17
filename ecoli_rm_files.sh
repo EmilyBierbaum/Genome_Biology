fc57ef9248595e01fcfade9c340c1ec672af00e8
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-09-17 00:47:23 -0500

    Add files via upload

---
+++ ecoli_rm_files.sh
@@ -0,0 +1,6 @@
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped.bam
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped.sam
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped_namesort.bam
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped_namesort_fixmate.bam
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped_namesort_fixmate_sort.bam
+rm /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped_sorted.bam

323949790cb43480dabf7659114ad0d6e6f75108
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-09-26 16:05:25 -0500

    Add files via upload

---
+++ unmapped_fastqc.sh
@@ -0,0 +1,2 @@
+fastqc /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r1.fastq -o /scratch/biol726301/BIOL7263_Genomics/fastqc_output/
+fastqc /scratch/biol726301/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r2.fastq -o /scratch/biol726301/BIOL7263_Genomics/fastqc_output/

718261da5d73e969ac809570b31613e7b86f705c
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-11-04 16:36:26 -0600

    Add files via upload

---
+++ pseud_long_quast.sh
@@ -0,0 +1,1 @@
+quast.py --output-dir /scratch/biol726301/BIOL7263_Genomics/pseudomonas_gm41/assembly/hybrid/quast /scratch/biol726301/BIOL7263_Genomics/pseudomonas_gm41/assembly/hybrid/contigs.fasta

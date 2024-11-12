9248f60ee10ed32476545d34dc2caaa9c2e9b24b
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-11-12 02:25:30 -0600

    Update seqkit.sh

--- seqkit.sh
+++ seqkit.sh
@@ -1,1 +1,1 @@
-seqkit replace -p "(.+)" -r '$1|{kv}' -k HEK_headers.txt transcripts.fasta > transcripts_annotated.fasta
+seqkit replace -p "(.+)" -r '$1|{kv}' -k C61_headers.txt transcripts_emult.fasta > Emult_transcripts_annotated.fasta

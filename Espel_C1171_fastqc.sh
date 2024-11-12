82c9f9cd8ad3d51be252e06e60a459bc5a6ad1ae
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-11-11 23:08:49 -0600

    Update Espel_C1171_fastqc.sh

--- Espel_C1171_fastqc.sh
+++ Espel_C1171_fastqc.sh
@@ -1,4 +1,2 @@
-mkdir -p /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output
-
-fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_C1171_R1.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/
-fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_C1171_R2.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/
+fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_C1172_R1.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/
+fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_C1172_R2.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/

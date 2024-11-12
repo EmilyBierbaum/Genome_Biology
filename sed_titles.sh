12d7b27078977471f049dd3e87982ccdcdd4234f
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-11-11 23:13:24 -0600

    Update sed_titles.sh

--- sed_titles.sh
+++ sed_titles.sh
@@ -1,1 +1,1 @@
-grep  '^>' updated_transcrip.fasta | sed -E 's/>([^ ]+) .* \(([^)]+)\).*/\1 \2/' > TTC_headers.txt
+grep  '^>' updated_transcriptome.fasta | sed -E 's/>([^ ]+) .* \(([^)]+)\).*/\1 \2/' > TTC_headers.txt

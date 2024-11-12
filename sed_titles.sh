29e686c99715ba4017cb92e0c1438a82449fdbd7
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-11-11 23:13:10 -0600

    Update sed_titles.sh

--- sed_titles.sh
+++ sed_titles.sh
@@ -1,3 +1,1 @@
-ln -s /scratch/biol726301/Eurycea_fastq/ updated_transcriptome.fasta ------DIDnt work :(
-
 grep  '^>' updated_transcrip.fasta | sed -E 's/>([^ ]+) .* \(([^)]+)\).*/\1 \2/' > TTC_headers.txt

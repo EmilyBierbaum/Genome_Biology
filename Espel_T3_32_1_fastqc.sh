97598cc384aa187c4544bad4ef30dc2131776808
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-12-09 17:37:39 -0600

    Add files via upload

---
+++ Espel_T3_32_1_fastqc.sh
@@ -0,0 +1,2 @@
+fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_T3_32_1_R1.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/
+fastqc /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastq/Espel_T3_32_1_R2.fastq.gz -o /scratch/biol726301/BIOL7263_Genomics/Eurycea_fastqc_output/

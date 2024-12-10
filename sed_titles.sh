bbeac2d2395d48e0e0c11cbee7012f3d74e1c559
Emily L. Bierbaum <71455209+EmilyBierbaum@users.noreply.github.com>
2024-12-09 19:32:22 -0600

    Update sed_titles.sh

--- sed_titles.sh
+++ sed_titles.sh
@@ -1,1 +1,1 @@
-grep  '^>' Espel_301_transcriptome.fasta | sed -E 's/>([^ ]+) .* \(([^)]+)\).*/\1 \2/' > TTC_headers.txt
+grep  '^>' Bonett_transcriptome.fasta | sed -E 's/>([^ ]+) .* \(([^)]+)\).*/\1 \2/' > TTC_headers.txt
